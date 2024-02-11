6967158a34ff893a2c141583bc017ad039c0edda
arkhaledwebdev <arkhaledwebdev@gmail.com>
2024-02-11 17:01:55 +0200

    Finish BFS and DFS

--- src/binary-search-tree/bst-tree.js
+++ src/binary-search-tree/bst-tree.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-param-reassign */
 const BSTNode = require("./bst-node");
 const prettyPrint = require("./pretty-print");
@@ -21,6 +22,29 @@ class BSTTree {
     rootNode.right = this.buildTree(arr, mid + 1, end);
 
     return rootNode;
+  }
+
+  find(value) {
+    let currentNode = this.root;
+
+    if (currentNode === null) {
+      return null;
+    }
+
+    while (currentNode) {
+      if (value < currentNode.data) {
+        currentNode = currentNode.left;
+      }
+
+      if (value > currentNode.data) {
+        currentNode = currentNode.right;
+      }
+
+      if (value === currentNode.data) {
+        break;
+      }
+    }
+    return currentNode;
   }
 
   insert(value) {
@@ -88,19 +112,162 @@ class BSTTree {
     }
     return this.findMinNode(node.left);
   }
+
+  height(node = this.root) {
+    if (node === null) return 0;
+    const leftHeight = this.height(node.left);
+    const rightHeight = this.height(node.right);
+    return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+  }
+
+  heightOfNode(value) {
+    return this.height(this.find(value));
+  }
+
+  depth(value) {
+    let currentNode = this.root;
+    let height = 0;
+    while (currentNode) {
+      if (value < currentNode.data) {
+        height += 1;
+        currentNode = currentNode.left;
+      }
+      if (value > currentNode.data) {
+        height += 1;
+        currentNode = currentNode.right;
+      }
+      if (value === currentNode.data) {
+        break;
+      }
+    }
+    return height;
+  }
+
+  levelOrder(callback) {
+    const queue = [];
+    const printQueue = [];
+    queue.push(this.root);
+
+    while (queue.length !== 0) {
+      const shiftedNode = queue.shift();
+      if (callback) {
+        callback(shiftedNode);
+      } else {
+        printQueue.push(shiftedNode.data);
+      }
+
+      if (shiftedNode.left != null) {
+        queue.push(shiftedNode.left);
+      }
+
+      if (shiftedNode.right != null) {
+        queue.push(shiftedNode.right);
+      }
+    }
+    if (callback) {
+      return undefined;
+    }
+    return printQueue;
+  }
+
+  levelOrderRecursive(callback, queue = [this.root], printQueue = []) {
+    if (queue.length === 0) return;
+
+    const shiftedNode = queue.shift();
+
+    if (callback) {
+      callback(shiftedNode);
+    } else {
+      printQueue.push(shiftedNode.data);
+    }
+
+    if (shiftedNode.left != null) {
+      queue.push(shiftedNode.left);
+    }
+
+    if (shiftedNode.right != null) {
+      queue.push(shiftedNode.right);
+    }
+
+    this.levelOrderRecursive(callback, queue, printQueue);
+
+    if (!callback) {
+      return printQueue;
+    }
+  }
+
+  preOrder(callback, printQueue = [], node = this.root) {
+    if (node === null) return;
+
+    if (callback) {
+      callback(node);
+    } else {
+      // add node to the array
+      printQueue.push(node.data);
+    }
+    // left
+    if (node.left) {
+      this.preOrder(callback, printQueue, node.left);
+    }
+    // right
+    if (node.right) {
+      this.preOrder(callback, printQueue, node.right);
+    }
+    if (!callback) {
+      return printQueue;
+    }
+  }
+
+  inOrder(callback, printQueue = [], node = this.root) {
+    if (node === null) return;
+    // left
+    this.inOrder(callback, printQueue, node.left);
+
+    if (callback) {
+      callback(node);
+    } else {
+      // add the node
+      printQueue.push(node.data);
+    }
+    // right
+    this.inOrder(callback, printQueue, node.right);
+
+    if (!callback) {
+      return printQueue;
+    }
+  }
+
+  postOrder(callback, printQueue = [], node = this.root) {
+    if (node === null) return;
+    // left
+    this.postOrder(callback, printQueue, node.left);
+    // right
+    this.postOrder(callback, printQueue, node.right);
+
+    if (callback) {
+      callback(node);
+    } else {
+      // add the node
+      printQueue.push(node.data);
+      return printQueue;
+    }
+  }
 }
 
 const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const testBTSTree = new BSTTree(testArray);
 
-console.log(testBTSTree);
-console.log(prettyPrint(testBTSTree.root));
-testBTSTree.insert(80);
-testBTSTree.insert(70);
+prettyPrint(testBTSTree.root);
 testBTSTree.insert(100);
-console.log(prettyPrint(testBTSTree.root));
-testBTSTree.delete(80);
-console.log(prettyPrint(testBTSTree.root));
-testBTSTree.insert(80);
-console.log(prettyPrint(testBTSTree.root));
+prettyPrint(testBTSTree.root);
+testBTSTree.delete(6345);
+prettyPrint(testBTSTree.root);
+console.log(testBTSTree.find(324));
+console.log(`height of (67) : ${testBTSTree.heightOfNode(67)}`);
+console.log(`depth of (67) : ${testBTSTree.depth(67)}`);
+console.log(`Level order : ${testBTSTree.levelOrder()}`);
+console.log(`Level order : ${testBTSTree.levelOrderRecursive()}`);
+console.log(`Pre-order : ${testBTSTree.preOrder()}`);
+console.log(`In-order : ${testBTSTree.inOrder()}`);
+console.log(`Post-order : ${testBTSTree.postOrder()}`);
